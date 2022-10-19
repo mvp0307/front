9be25284d9da59c39c5d7e8ed3c6af9ac44a6442
avagyan6686 <avagyan6686@gmail.com>
2022-10-20 00:35:42 +0400

    feat/header: Created Header

---
+++ src/assets/svgs/MetamaskSvg.tsx
@@ -0,0 +1,213 @@
+import React from 'react';
+
+const MetamaskSvg = () => {
+  return (
+    <svg width="26" height="24" viewBox="0 0 26 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M23.8139 1.00012L14.4384 7.87507L16.1818 3.82343L23.8139 1.00012Z"
+        fill="#E17726"
+        stroke="#E17726"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M2.18509 1.00061L11.4772 7.93972L9.81719 3.82392L2.18509 1.00061Z"
+        fill="#E27625"
+        stroke="#E27625"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M20.437 16.941L17.9424 20.7177L23.284 22.1752L24.8141 17.0235L20.437 16.941Z"
+        fill="#E27625"
+        stroke="#E27625"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M1.19391 17.0235L2.71477 22.1752L8.04704 20.7177L5.56173 16.941L1.19391 17.0235Z"
+        fill="#E27625"
+        stroke="#E27625"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M7.75967 10.5618L6.27591 12.7801L11.5618 13.0184L11.3856 7.38098L7.75967 10.5618Z"
+        fill="#E27625"
+        stroke="#E27625"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M18.2377 10.5615L14.5562 7.31653L14.4356 13.0182L19.7215 12.7798L18.2377 10.5615Z"
+        fill="#E27625"
+        stroke="#E27625"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M8.04691 20.7181L11.2463 19.1873L8.49203 17.0607L8.04691 20.7181Z"
+        fill="#E27625"
+        stroke="#E27625"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M14.7524 19.1873L17.9425 20.7181L17.5067 17.0607L14.7524 19.1873Z"
+        fill="#E27625"
+        stroke="#E27625"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M17.943 20.7175L14.7529 19.1866L15.0126 21.24L14.9848 22.1108L17.943 20.7175Z"
+        fill="#D5BFB2"
+        stroke="#D5BFB2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M8.04929 20.7184L11.0168 22.1118L10.9983 21.2409L11.2486 19.1876L8.04929 20.7184Z"
+        fill="#D5BFB2"
+        stroke="#D5BFB2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M11.0718 15.7032L8.41953 14.9332L10.2928 14.0807L11.0718 15.7032Z"
+        fill="#233447"
+        stroke="#233447"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M14.9307 15.7032L15.7096 14.0807L17.5922 14.9332L14.9307 15.7032Z"
+        fill="#233447"
+        stroke="#233447"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M8.04731 20.7177L8.51099 16.941L5.56201 17.0235L8.04731 20.7177Z"
+        fill="#CC6228"
+        stroke="#CC6228"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M17.4879 16.941L17.9423 20.7177L20.4368 17.0235L17.4879 16.941Z"
+        fill="#CC6228"
+        stroke="#CC6228"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M19.7224 12.7794L14.4365 13.0178L14.928 15.7036L15.707 14.0811L17.5895 14.9336L19.7224 12.7794Z"
+        fill="#CC6228"
+        stroke="#CC6228"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M8.41708 14.9336L10.2903 14.0811L11.0693 15.7036L11.5608 13.0178L6.2749 12.7794L8.41708 14.9336Z"
+        fill="#CC6228"
+        stroke="#CC6228"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M6.27777 12.7799L8.49414 17.0607L8.41995 14.9341L6.27777 12.7799Z"
+        fill="#E27525"
+        stroke="#E27525"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M17.5902 14.9336L17.5068 17.0602L19.7231 12.7794L17.5902 14.9336Z"
+        fill="#E27525"
+        stroke="#E27525"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M11.5622 13.0177L11.0707 15.7035L11.6921 18.8752L11.8312 14.6952L11.5622 13.0177Z"
+        fill="#E27525"
+        stroke="#E27525"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M14.4374 13.0177L14.1777 14.686L14.3076 18.8752L14.9289 15.7035L14.4374 13.0177Z"
+        fill="#E27525"
+        stroke="#E27525"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M14.9294 15.7037L14.3081 18.8754L14.7532 19.187L17.5075 17.0604L17.5909 14.9337L14.9294 15.7037Z"
+        fill="#F5841F"
+        stroke="#F5841F"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M8.41953 14.9337L8.49371 17.0604L11.2479 19.187L11.6931 18.8754L11.0718 15.7037L8.41953 14.9337Z"
+        fill="#F5841F"
+        stroke="#F5841F"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M14.9859 22.1107L15.0137 21.2399L14.7726 21.0382H11.2301L10.9983 21.2399L11.0168 22.1107L8.04932 20.7174L9.08795 21.5607L11.193 22.9999H14.8004L16.9148 21.5607L17.9442 20.7174L14.9859 22.1107Z"
+        fill="#C0AC9D"
+        stroke="#C0AC9D"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M14.7542 19.1868L14.3091 18.8751H11.694L11.2489 19.1868L10.9985 21.2401L11.2303 21.0384H14.7728L15.0139 21.2401L14.7542 19.1868Z"
+        fill="#161616"
+        stroke="#161616"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M24.2119 8.32423L25.0002 4.52926L23.8132 1.00012L14.7529 7.6459L18.2398 10.5609L23.164 11.9817L24.249 10.7259L23.7761 10.3867L24.5272 9.70839L23.9523 9.26839L24.7034 8.70006L24.2119 8.32423Z"
+        fill="#763E1A"
+        stroke="#763E1A"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M1 4.52975L1.79752 8.32472L1.28748 8.70055L2.04791 9.26888L1.47295 9.70888L2.2241 10.3872L1.75115 10.7264L2.83616 11.9822L7.76039 10.5614L11.2472 7.64639L2.18701 1.00061L1 4.52975Z"
+        fill="#763E1A"
+        stroke="#763E1A"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M23.1627 11.9825L18.2385 10.5616L19.7222 12.78L17.5059 17.0608L20.4363 17.0241H24.8134L23.1627 11.9825Z"
+        fill="#F5841F"
+        stroke="#F5841F"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M7.75956 10.5616L2.83532 11.9825L1.19391 17.0241H5.56173L8.49217 17.0608L6.27579 12.78L7.75956 10.5616Z"
+        fill="#F5841F"
+        stroke="#F5841F"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M14.4366 13.0179L14.7519 7.64632L16.18 3.82385H9.81839L11.2465 7.64632L11.5618 13.0179L11.6824 14.7046L11.6916 18.8754H14.3068L14.316 14.7046L14.4366 13.0179Z"
+        fill="#F5841F"
+        stroke="#F5841F"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default MetamaskSvg;
--- src/features/Header/Header.tsx
+++ src/features/Header/Header.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
 
+import Logo from '../../assets/svgs/Logo';
 import ConnectWallet from '../../components/ConnectWallet/ConnectWallet';
+import styles from './header.module.scss';
 
 const Header: React.FC = () => {
-  return <ConnectWallet />;
+  return (
+    <header className={styles.header}>
+      <div className="container">
+        <nav className={styles.nav}>
+          <a href="/">
+            <Logo />
+          </a>
+          <menu className={styles.menu}>
+            <a className={styles.menuItem} href="">
+              Gross
+            </a>
+            <a className={styles.menuItem} href="">
+              Investment
+            </a>
+            <ConnectWallet />
+          </menu>
+        </nav>
+      </div>
+    </header>
+  );
 };
 export default React.memo(Header);
