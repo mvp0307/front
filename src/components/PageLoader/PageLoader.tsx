e5a6f6c8e520616178639d1b62287ac42fa703ec
avagyan6686 <avagyan6686@gmail.com>
2022-11-02 20:48:18 +0400

    investitions blocks

--- src/components/PageLoader/PageLoader.tsx
+++ src/components/PageLoader/PageLoader.tsx
@@ -3,6 +3,10 @@ import React from 'react';
 import styles from './PageLoader.module.scss';
 
 const PageLoader: React.FC = () => {
-  return <div className={styles.spinner}></div>;
+  return (
+    <div className={styles.loader}>
+      <div className={styles.spinner}></div>
+    </div>
+  );
 };
 export default React.memo(PageLoader);
