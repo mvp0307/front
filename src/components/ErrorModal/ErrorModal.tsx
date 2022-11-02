bf45bf211c8fda011a043973c52556ba7fe8b6ac
avagyan6686 <avagyan6686@gmail.com>
2022-11-02 21:03:40 +0400

    error and success modals

--- src/components/ErrorModal/ErrorModal.tsx
+++ src/components/ErrorModal/ErrorModal.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
 
+import RejectedIconSvg from '../../assets/svgs/RejectedIconSvg';
+import styles from './errorModal.module.scss';
+
 const ErrorModal: React.FC = () => {
-  return <div>ErrorModal</div>;
+  return (
+    <div className={styles.modalContent}>
+      <div className={styles.modalBody}>
+        <h2 className={styles.modalTitle}>Investment Rejected</h2>
+        <div className={styles.modaltext}>
+          <p>You dont have enough money</p>
+        </div>
+        <div className={styles.modalIcon}>
+          <RejectedIconSvg />
+        </div>
+        <div className={styles.modalBtn}>
+          <button className={styles.outlineBtn} type="button">
+            Dismiss
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ErrorModal;
